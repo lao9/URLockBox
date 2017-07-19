d67b5156732ec57246efb584770a7517a6413fc4
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-19 11:50:30 -0600

    add user and link migrations, models, and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,72 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create Users
+10.times do |n|
+  email = "lauren@lauren.com" if n.zero?
+  email = Faker::Internet.email unless n.zero?
+  User.create(
+    email: email,
+    password: "password"
+  )
+  puts "Creater User ##{User.last.id}, email: #{User.last.email}"
+end
+
+## Create Links
+valid_urls = [
+  "https://mail.google.com",
+  "https://en.todoist.com",
+  "https://calendar.google.com",
+  "https://drive.google.com",
+  "http://backend.turing.io/module4/",
+  "https://github.com",
+  "http://ruby-doc.org/",
+  "https://turing.skedda.com",
+  "https://www.fabricationgem.org/",
+  "http://getbootstrap.com/getting-started/",
+  "http://ncase.me/polygons/",
+  "https://turing-fridays.firebaseapp.com/",
+  "https://apidock.com/ruby/DateTime/strftime",
+  "https://stackoverflow.com/",
+  "https://www.codewars.com/dashboard",
+  "http://bencrowder.net/files/vim-fu/",
+  "http://bigocheatsheet.com/",
+  "http://api.jquery.com/",
+  "http://www.usatf.org/routes/map/"
+]
+
+url_titles = [
+  "Gmail",
+  "ToDoist",
+  "Google Calendar",
+  "Google Drive",
+  "Turing BE Mod 4 Curriculum",
+  "Github",
+  "Ruby Docs",
+  "Turing Room Scheduler",
+  "Fabricator Gem Docs",
+  "Bootstrap Getting Started",
+  "Parable of Polygons",
+  "Turing Fridays",
+  "Ruby Stftime Doc",
+  "Stack Overflow",
+  "Code Wars",
+  "Vim Cheat Sheet",
+  "Big O Cheat Sheet",
+  "JQuery Docs",
+  "Run Mapper"
+]
+
+## Assign links to users
+User.all.each do |user|
+  3.times do
+    n = rand(0...valid_urls.length)
+    Link.create(
+      title: url_titles[n],
+      url: valid_urls[n],
+      user: user
+    )
+    puts "Added #{Link.last.title} to User #{user.email}"
+  end
+end
---
+++ spec/factories/create_links.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :create_link do
+    title "MyString"
+    url "MyString"
+    read false
+    user nil
+  end
+end
