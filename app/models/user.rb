d3c9f77a5b3e293d9e3421f53932fb27967b13a3
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-19 12:19:31 -0600

    add model tests for user and link

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :links
+
+  validates :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {create(:user)}
+  let(:user_with_links) {create(:user_with_links)}
+
+  describe "attributes" do
+    it "responds to email" do
+      expect(user).to respond_to(:email)
+    end
+    it "responds to password digest" do
+      expect(user).to respond_to(:password_digest)
+    end
+  end
+
+  describe "validations" do
+    it { expect(user).to validate_presence_of(:email)}
+  end
+
+  describe "relationships" do
+    it "has many links" do
+      should have_many(:links)
+    end
+    it "has valid factory with links associations" do
+      expect(user_with_links).to respond_to(:links)
+      expect(user_with_links.links.count).to eq(3)
+      expect(user_with_links.links.first).to be_a(Link)
+    end
+  end
 end
