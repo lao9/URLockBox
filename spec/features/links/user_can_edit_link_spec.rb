5e34f279d7983fe9130d0cfe8d2bf2c12793cc54
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-27 10:00:45 -0600

    clean up edge case testing for edit links

--- spec/features/links/user_can_edit_link_spec.rb
+++ spec/features/links/user_can_edit_link_spec.rb
@@ -16,16 +16,14 @@ feature "add new link", :js => true do
 
       expect(current_path).to eq(edit_link_path(Link.find(1)))
 
-      within ".link-edit" do
-        fill_in "link[url]", with: "https://github.com"
-        fill_in "link[title]", with: "Github"
-        click_on "Update Link"
-      end
+      fill_in "link[url]", with: "https://github.com"
+      fill_in "link[title]", with: "Github"
+      click_on "Update Link"
 
       expect(page).to have_content("Successfully updated link!")
 
       expect(current_path).to eq(links_path)
-      
+
       within "#link-1" do
         expect(page).to have_content("Github")
         expect(page).to have_link("https://github.com")
@@ -37,100 +35,105 @@ feature "add new link", :js => true do
     end
   end
   context "as authenticated user with invalid params" do
-    xscenario "cannot add an invalid url" do
+    scenario "cannot add an invalid url" do
       allow_any_instance_of(ApplicationController)
         .to receive(:current_user)
         .and_return(user)
 
       visit root_path
 
-      within ".link-form" do
-        fill_in "link[url]", with: "turing.io"
-        fill_in "link[title]", with: "Turing"
-        click_on "Add Link"
+      within "#link-1" do
+        click_on "Edit"
       end
+
+      fill_in "link[url]", with: "github.com"
+      fill_in "link[title]", with: "Github"
+      click_on "Update Link"
 
       expect(page).to have_content("Invalid URL!")
-
-      within ".links" do
-        expect(page).to have_selector(".link", count: 3)
-      end
+      expect(page).to have_content("Edit Link")
+      expect(Link.find(1).url).to eq("http://ruby-doc.org/")
+      expect(Link.find(1).title).to eq("Ruby Docs")
     end
-    xscenario "cannot add missing url" do
+    scenario "cannot add missing url" do
       allow_any_instance_of(ApplicationController)
-      .to receive(:current_user)
-      .and_return(user)
+        .to receive(:current_user)
+        .and_return(user)
 
       visit root_path
 
-      within ".link-form" do
-        fill_in "link[url]", with: ""
-        fill_in "link[title]", with: "Turing"
-        click_on "Add Link"
+      within "#link-1" do
+        click_on "Edit"
       end
 
-      expect(page).to have_content("Missing URL!")
+      fill_in "link[url]", with: ""
+      fill_in "link[title]", with: "Github"
+      click_on "Update Link"
 
-      within ".links" do
-        expect(page).to have_selector(".link", count: 3)
-      end
+      expect(page).to have_content("Invalid URL! Missing URL!")
+      expect(page).to have_content("Edit Link")
+      expect(Link.find(1).url).to eq("http://ruby-doc.org/")
+      expect(Link.find(1).title).to eq("Ruby Docs")
     end
-    xscenario "cannot add missing title" do
+    scenario "cannot add missing title" do
       allow_any_instance_of(ApplicationController)
-      .to receive(:current_user)
-      .and_return(user)
+        .to receive(:current_user)
+        .and_return(user)
 
       visit root_path
 
-      within ".link-form" do
-        fill_in "link[url]", with: "http://turing.io"
-        fill_in "link[title]", with: ""
-        click_on "Add Link"
+      within "#link-1" do
+        click_on "Edit"
       end
+
+      fill_in "link[url]", with: "https://github.com"
+      fill_in "link[title]", with: ""
+      click_on "Update Link"
 
       expect(page).to have_content("Missing Title!")
-
-      within ".links" do
-        expect(page).to have_selector(".link", count: 3)
-      end
+      expect(page).to have_content("Edit Link")
+      expect(Link.find(1).url).to eq("http://ruby-doc.org/")
+      expect(Link.find(1).title).to eq("Ruby Docs")
     end
-    xscenario "cannot add invalid link OR title" do
+    scenario "cannot add invalid link OR title" do
       allow_any_instance_of(ApplicationController)
-      .to receive(:current_user)
-      .and_return(user)
+        .to receive(:current_user)
+        .and_return(user)
 
       visit root_path
 
-      within ".link-form" do
-        fill_in "link[url]", with: "turing.io"
-        fill_in "link[title]", with: ""
-        click_on "Add Link"
+      within "#link-1" do
+        click_on "Edit"
       end
+
+      fill_in "link[url]", with: "github.com"
+      fill_in "link[title]", with: ""
+      click_on "Update Link"
 
       expect(page).to have_content("Invalid URL! Missing Title!")
-
-      within ".links" do
-        expect(page).to have_selector(".link", count: 3)
-      end
+      expect(page).to have_content("Edit Link")
+      expect(Link.find(1).url).to eq("http://ruby-doc.org/")
+      expect(Link.find(1).title).to eq("Ruby Docs")
     end
-    xscenario "cannot add missing link OR title" do
+    scenario "cannot add missing link OR title" do
       allow_any_instance_of(ApplicationController)
-      .to receive(:current_user)
-      .and_return(user)
+        .to receive(:current_user)
+        .and_return(user)
 
       visit root_path
 
-      within ".link-form" do
-        fill_in "link[url]", with: ""
-        fill_in "link[title]", with: ""
-        click_on "Add Link"
+      within "#link-1" do
+        click_on "Edit"
       end
 
-      expect(page).to have_content("Missing URL! Missing Title!")
+      fill_in "link[url]", with: ""
+      fill_in "link[title]", with: ""
+      click_on "Update Link"
 
-      within ".links" do
-        expect(page).to have_selector(".link", count: 3)
-      end
+      expect(page).to have_content("Invalid URL! Missing URL! Missing Title!")
+      expect(page).to have_content("Edit Link")
+      expect(Link.find(1).url).to eq("http://ruby-doc.org/")
+      expect(Link.find(1).title).to eq("Ruby Docs")
     end
   end
 end
