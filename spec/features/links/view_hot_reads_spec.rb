84f2161c066301ab2277acb0e1d71fd4e50982ec
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-27 16:05:11 -0600

    add test for when reads load

---
+++ spec/features/links/view_hot_reads_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+feature "top and hot links" do
+  let(:user) {create(:user_with_a_link)}
+  before :each do
+    host = "https://loliveri-hotreads.herokuapp.com"
+    get "#{host}/api/v1/reset"
+    user.links.create(title: "Bootstrap Getting Started", url: "http://getbootstrap.com/getting-started/")
+    user.links.create(title: "Turing Room Scheduler", url: "https://turing.skedda.com")
+
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user)
+      .and_return(user)
+
+    visit root_path
+  end
+  scenario "user with an existing top or hot link is displayed" do
+    link1, link2, link3 = user.links
+
+    within "#link-#{link1.id}" do
+      within ".top" do
+        expect(page).to have_content("TOP HOT READ!!")
+        expect(page).to_not have_content("SPICY READ!!")
+      end
+    end
+
+    within "#link-#{link2.id}" do
+      within ".spicy" do
+        expect(page).to_not have_content("TOP HOT READ!!")
+        expect(page).to have_content("SPICY READ!!")
+      end
+    end
+
+    within "#link-#{link3.id}" do
+      expect(page).to_not have_content("TOP HOT READ!!")
+      expect(page).to_not have_content("SPICY READ!!")
+    end
+  end
+  xscenario "user can mark new link as red and it will become hot" do
+
+  end
+end
